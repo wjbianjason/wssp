ca514b686310e98d6249fea41179931e6ecd2e7c
wjbianjason <wjbianjason@163.com>
2021-07-28 00:46:10 +0800

    imp

--- binance_local.py
+++ binance_local.py
@@ -6,7 +6,7 @@ basePeriod: 30m
 exchanges: [{"eid":"BINANCE","currency":"LTC_USDT","balance":500,"stocks":0}]
 '''
 import time
-from pyecharts.charts import Bar
+# from pyecharts.charts import Bar
 from binance.client import Client
 from fmz import *
 task = VCtx(__doc__) 
@@ -14,8 +14,9 @@ task = VCtx(__doc__)
 recent_value = -1  #avoid special case
 rollback_rate = 0.05
 buy_side_price = -1
-deal_record = []
-action_record = []
+close_trade = -1
+# deal_record = []
+# action_record = []
 
 
 # 获取历史分钟级数据模拟回测数据
@@ -25,133 +26,184 @@ action_record = []
 # 选择开盘价买入
 
 class OffAccount:
-    def __init__(self,init_balance,init_stocks):
-        self.balance = init_balance
+    def __init__(self, symbol, init_balance,init_stocks):
+        self.binance = init_balance
         self.stocks = init_stocks
+        self.symbol = symbol
     
     def g_account(self, price):
-        return self.balance+price*self.stocks
+        return self.binance+price*self.stocks
 
-    def buy(self, number, price):
-        self.stocks += number
+    def get_binance(self):
+        return self.stocks
+
+    def buy(self, price):
+        number = self.binance/price 
+        self.stocks +=  number 
         self.binance -= number*price
+        return "true"
 
     def sell(self, number, price):
         self.stocks -= number
         self.binance += number*price
+        self.log("Sell Total Money"+str(self.binance))
 
+    def log(self, content):
+        with open('logs/reve.log','a') as f:
+            f.write(content)
 
 class OlineAccount:
-    def __init__(self,client, init_balance,init_stocks):
+    def __init__(self,client, symbol, init_balance,init_stocks):
         self.client = client
-        self.balance = init_balance
-        self.stocks = init_stocks
+        self.symbol = symbol  
+        self.pre = 4     
     
-    def g_account(self, price):
-        return self.balance+price*self.stocks
+    def get_usdt(self):
+        dic = self.client.get_asset_balance(asset='USDT')
+        return dic['free']
 
-    def buy(self, number, price):
-        self.stocks += number
-        self.binance -= number*price
+    def get_binance(self):
+        dic = self.client.get_asset_balance(asset='BNB')
+        return dic['free']
+
+    def buy(self, price):
+        number = round(self.get_usdt()/price, self.pre)
+        order_id = self.client.order_limit_buy(
+                    symbol=self.symbol,
+                    quantity=number,
+                    price='0.00001')
+        return true
+        # if order_id is None:
+            # self.client.get_all_orders(symbol=self.symbol)
 
     def sell(self, number, price):
-        self.stocks -= number
-        self.binance += number*price
+        number = self.get_binance()
+        order_id = self.client.order_limit_sell(
+                    symbol=self.symbol,
+                    quantity=number,
+                    price='0.00001')        
 
+        self.log("Sell Total Money"+str(self.get_usdt()))        
+
+    def log(self, content):
+        with open('logs/reve.log','a') as f:
+            f.write(content)
 
 
 class BinanceExchange:
     def __init__(self, status, init_balance, init_stocks, symbol, recall_start, recall_end=None):
         self.status = status
-        self.fake_account = OffAccount(init_balance,init_stocks)
-        api_key = ''
-        api_secret = '' 
-        self.client =  Client(api_key, api_secret)  
+        api_key = '9CuLKi24uTNSpfMbDo93MRZNcMzzGMgSfXI9CtXcru'
+        api_secret = 'ZbiVrjFqdUs1KEJCmvuIJ2hjAut8LJzwU5jkGDR1aDwhnRguuxxCD2QdNluoRZBI' 
+        self.client =  Client(api_key, api_secret) 
         self.symbol = symbol
-        self.hist_k_line = self.client.get_historical_klines_generator(self.symbol, Client.KLINE_INTERVAL_1MINUTE, recall_start, recall_end)
+        if status == "offline":
+            self.hist_k_line = self.client.get_historical_klines_generator(self.symbol, Client.KLINE_INTERVAL_1MINUTE, recall_start, recall_end)            
+            self.account = OffAccount(symbol, init_balance, init_stocks)
+        elif status == "online":
+            self.account = OlineAccount(self.client, init_balance, init_stocks)
         self.two_day_gap = 48 * 60 * 60 * 1000
+        self.one_day_gap = 24 * 60 * 60 * 1000
         self.one_min_gap = 60 * 1000
     
 
     def get_ticker(self):
-        ticker_info = ['Open_T','Open','High','Low','Close','Volume','Close_T', 'Quote', 'Num', '_V1','_V2','_V3']
         if self.status == 'offline':
+            ticker_info = ['Open_T','Open','High','Low','price','Volume','Close_T', 'Quote', 'Num', '_V1','_V2','_V3']            
             info_dict = dict(zip(ticker_info, next(self.hist_k_line)))
-            recent_k_lines = [float(t[4]) for t in self.client.get_historical_klines(self.symbol, Client.KLINE_INTERVAL_15MINUTE, info_dict['Open_T']-self.two_day_gap, info_dict['Open_T']-self.one_min_gap)]
+            recent_k_lines = [float(t[4]) for t in self.client.get_historical_klines(self.symbol, Client.KLINE_INTERVAL_15MINUTE, info_dict['Open_T']-self.one_day_gap, info_dict['Open_T']-self.one_min_gap)]
             # print(recent_k_lines)
 
-            EMA_25 = TA.EMA(recent_k_lines, 25)[-1]
-            EMA_99 = TA.EMA(recent_k_lines, 99)[-1]
+            EMA_14 = TA.EMA(recent_k_lines, 14)[-1]
+            EMA_60 = TA.EMA(recent_k_lines, 60)[-1]
             BOLL = TA.BOLL(recent_k_lines)
-            info_dict.setdefault('EMA_25', EMA_25)
-            info_dict.setdefault('EMA_99', EMA_99)
+            info_dict.setdefault('EMA_14', EMA_14)
+            info_dict.setdefault('EMA_60', EMA_60)
             Boll_Bound = [BOLL[0][-1], BOLL[1][-1], BOLL[2][-1]]
-            Boll_Var_Rate = (BOLL[2][-1] - BOLL[0][-1]) / BOLL[1][-1]
+            Boll_Var_Rate = (BOLL[0][-1] - BOLL[2][-1]) / BOLL[1][-1]
             Boll_Bound.append(Boll_Var_Rate)
             info_dict.setdefault('BOLL', Boll_Bound)
+            info_dict['price'] = float(info_dict['price'])
+            # info_dict.setdefault('Hist_T', recent_k_lines[0][1])
+            return info_dict
+        elif self.status == "online":
+            info_dict = client.get_recent_trades(symbol=self.symbol, limit=1)[0]
+            recent_k_lines = [float(t[4]) for t in self.client.get_historical_klines(self.symbol, Client.KLINE_INTERVAL_15MINUTE, info_dict['time']-self.one_day_gap, info_dict['time']-self.one_min_gap)]
+            EMA_14 = TA.EMA(recent_k_lines, 14)[-1]
+            EMA_60 = TA.EMA(recent_k_lines, 60)[-1]
+            BOLL = TA.BOLL(recent_k_lines)
+            info_dict.setdefault('EMA_14', EMA_14)
+            info_dict.setdefault('EMA_60', EMA_60)
+            Boll_Bound = [BOLL[0][-1], BOLL[1][-1], BOLL[2][-1]]
+            Boll_Var_Rate = (BOLL[0][-1] - BOLL[2][-1]) / BOLL[1][-1]
+            Boll_Bound.append(Boll_Var_Rate)
+            info_dict.setdefault('BOLL', Boll_Bound)
+            info_dict['price'] = float(info_dict['price'])
             # info_dict.setdefault('Hist_T', recent_k_lines[0][1])
             return info_dict
 
 
 
-    def boll_trade():
-        global buy_side_price,deal_record,action_record
-        print("BOLL状态"+str(BOLL[0][-1])+","+str(BOLL[1][-1])+","+str(BOLL[2][-1]))    
-        # if(((recent_value - NowTicker.Last)/recent_value > rollback_rate) or (EMA_25 <= EMA_99)): 
-        if (recent_value - NowTicker.Last)/recent_value > rollback_rate: 
-        # if ((recent_value - NowTicker.Last)/recent_value > rollback_rate) or (EMA_25 < EMA_99) or (buy_side_price >= BOLL[0][-1]): 
-            if((recent_value - NowTicker.Last)/recent_value > rollback_rate):
-                print("紧急情况,股价骤跌")
-            if((EMA_25 <= EMA_99)):
-                print("紧急情况,下跌态势")            
-            print("股价波动比例"+str((recent_value - NowTicker.Last)/recent_value))
-            print("下行趋势"+str(EMA_25)+","+str(EMA_99))   
-            if buy_side_price != -1:
-                print("买入价",buy_side_price,"卖出",NowAsset.Stocks,"个币")
-                # while 1:  #实际上线则全部卖掉然后待机
-                exchange.Sell(NowTicker.Buy,NowAsset.Stocks)
-                buy_side_price = -1        
-                deal_record.append(NowTicker.Buy)
-                action_record.append("下")
+    def boll_trade(self, ticker_info):
+        global buy_side_price, rollback_rate
+        # self.log("BOLL状态"+str(ticker_info['BOLL'])) 
+        self.log("ticket状态"+str(ticker_info)) 
+
+        # print(ticker_info)
+        # print(buy_side_price)
+        if ((((buy_side_price - ticker_info['price'])/buy_side_price > rollback_rate) or (ticker_info['EMA_14'] < ticker_info['EMA_60']) or (ticker_info['price'] > ticker_info['BOLL'][0]) ) and buy_side_price != -1): 
+            
+            if((buy_side_price - ticker_info['price'])/buy_side_price > rollback_rate):
+                self.log("股价波动比例"+str((buy_side_price - ticker_info['price'])/buy_side_price))
+                self.log("紧急情况,股价骤跌,卖出"+str(self.account.stocks))
+                self.account.sell(self.account.get_binance(), ticker_info['price'])
+                buy_side_price = -1  
+
+            if(ticker_info['EMA_14'] < ticker_info['EMA_60']):
+                self.log("紧急情况,下跌态势,卖出"+str(self.account.stocks))
+                self.account.sell(self.account.get_binance(), ticker_info['price'])
+                buy_side_price = -1  
+
+            if(ticker_info['price'] > ticker_info['BOLL'][0]):
+                self.log("BOLL上界,卖出"+str(self.account.stocks))
+                self.account.sell(self.account.get_binance(), ticker_info['price'])
+                buy_side_price = -1 
 
         else:
-            if(NowTicker.Last < BOLL[2][-1] and NowTicker.Volume >= 2 and NowAsset.Balance > 0.1 and buy_side_price < 0):
-                print("交易将被执行，以",NowTicker.Sell,"买入",NowAsset.Balance/NowTicker.Sell,"个币")
-                exchange.SetPrecision(5,5) #设置计价精度
-                exchange.Buy(NowTicker.Sell,NowAsset.Balance/NowTicker.Sell-0.01) #执行币买入交易 
-                buy_side_price =  NowTicker.Sell 
-                deal_record.append(NowTicker.Sell)
-                action_record.append("上")            
-                CancelAll()     
-            elif(NowTicker.Last > BOLL[0][-1] and NowTicker.Volume >= 100  and NowAsset.Stocks > 0 and buy_side_price > 0):
-                print("买入价",buy_side_price)
-                print("交易将被执行，以",NowTicker.Buy,"卖出",NowAsset.Stocks,"个币")
-                exchange.SetPrecision(5,5) #设置计价精度
-                exchange.Sell(NowTicker.Buy,NowAsset.Stocks) #执行币卖出交易
-                buy_side_price = -1
-                deal_record.append(NowTicker.Buy)
-                action_record.append("下")
-                CancelAll()
-
+            if(ticker_info['price'] < ticker_info['BOLL'][2] and ticker_info['BOLL'][3] > 0.04  and buy_side_price < 0 and (ticker_info['EMA_14'] > ticker_info['EMA_60'])):
+                self.log("交易将被执行，以"+str(ticker_info['price'])+"买入")
+                stag = self.account.buy(ticker_info['price'])
+                if stag:
+                    buy_side_price = ticker_info['price']
             else:
-                print("未触发交易条件")
+                self.log("未触发交易条件")
 
 
-    def log(self, content, stag):
-        if stag == 'act':
-            with open('logs/act.log','a') as f:
-                f.write(content)
-        elif stag == 'reve':
-            with open('logs/reve.log','a') as f:
-                f.write(content)
+    def log(self, content):
+        with open('logs/act.log','w') as f:
+            f.write(content+"\n")
 
-                
 
+    def trade_iterator(self):
+        if self.status == "offline":
+            for i in range(1000):
+                time.sleep(0.1)
+                ticker_info = self.get_ticker()
+                self.boll_trade(ticker_info)
+        elif self.status == "online":
+            while true:
+                time.sleep(1)
+                ticker_info = self.get_ticker()
+                self.boll_trade(ticker_info)
 
 if __name__ == '__main__':
     # b_e = BinanceExchange("offline",10000,0,"BNBUSDT","1 Mar, 2021", "30 Mar, 2021")
-    b_e = BinanceExchange("offline",10000,0,"BNBUSDT",1614556800000, 1615577800000)
-    print(b_e.get_ticker())
+    b_e = BinanceExchange("offline",10000,0,"BNBUSDT",1612622774000, 1626622774000)
+    b_e.trade_iterator()
+
+
+
+    # print(b_e.get_ticker())
     # print(b_e.get_ticker())
     # print(b_e.get_ticker())
     # print(b_e.get_ticker())
